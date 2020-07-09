7022a3a71982eed6ebab90aaeb7c41779321675c
william-cesar <william.cesar.mg@hotmail.com>
2020-07-09 08:59:37 -0300

    Updates

---
+++ precache-manifest.7c3a2c7847ee1ce8f6abdb8d1146dc50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a61103d4548546582e0f686a590e890",
+    "url": "/RepoSearch/index.html"
+  },
+  {
+    "revision": "af3e50745aa1441e2bd4",
+    "url": "/RepoSearch/static/css/main.19a2e726.chunk.css"
+  },
+  {
+    "revision": "b05632b3baa664e242ff",
+    "url": "/RepoSearch/static/js/2.42bcb4bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RepoSearch/static/js/2.42bcb4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3e50745aa1441e2bd4",
+    "url": "/RepoSearch/static/js/main.381f9119.chunk.js"
+  },
+  {
+    "revision": "45986efaa766e503fdfa",
+    "url": "/RepoSearch/static/js/runtime-main.49ac6105.js"
+  }
+]);
